11d4e757000bd473e6790e9d025ce6d2e73524fa
s41blizzard <s41.blizzard@mail.ru>
2018-09-08 16:59:58 +0300

    -

--- app/blogengine/blog/urls.py
+++ app/blogengine/blog/urls.py
@@ -6,5 +6,5 @@ urlpatterns = [
     path('', posts_list, name='posts_list_url'),
     path('post/<str:slug>/', post_details, name='post_details_url'),
     path('tags/', tags_list, name='tags_list_url'),
-    path('tag/<str:slug', tag_detail, name='tag_detail_url')
+    path('tag/<str:slug>', tag_detail, name='tag_detail_url')
 ]
