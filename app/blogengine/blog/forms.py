c071d9b422fdfae02ea625178db7453dd27d29eb
s41blizzard <s41.blizzard@mail.ru>
2018-09-09 23:15:36 +0300

    -

--- app/blogengine/blog/forms.py
+++ app/blogengine/blog/forms.py
@@ -5,11 +5,6 @@ from .models import Tag
 
 
 class TagForm(forms.ModelForm):
-    # title = forms.CharField(max_length=50)
-    # slug = forms.CharField(max_length=50)
-    #
-    # title.widget.attrs.update({'class': 'form-control'})
-    # slug.widget.attrs.update({'class': 'form-control'})
     class Meta:
         model = Tag
         fields = ['title', 'slug']
@@ -26,7 +21,3 @@ class TagForm(forms.ModelForm):
         if Tag.objects.filter(slug__iexact=new_slug).count():
             raise ValidationError('Slug has to be unique ')
         return new_slug
-
-    # def save(self):
-    #     new_tag = Tag.objects.create(title=self.cleaned_data['title'], slug=self.cleaned_data['slug'])
-    #     return new_tag
