9dae504ea34be35d1f4e46b5a46ccd0420a605cb
s41blizzard <s41.blizzard@mail.ru>
2018-09-16 11:47:15 +0300

    -

--- app/blogengine/blog/urls.py
+++ app/blogengine/blog/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('tag/create/', TagCreate.as_view(), name='tag_create_url'),
     path('tags/', tags_list, name='tags_list_url'),
     path('tag/<str:slug>', TagDetail.as_view(), name='tag_detail_url'),
-    path('tag/<str:slug>/update/', TagUpdate.as_view(), name='tag_update_url')
+    path('tag/<str:slug>/update/', TagUpdate.as_view(), name='tag_update_url'),
+    path('tag/<str:slug>/delete/', TagDelete.as_view(), name='tag_delete_url')
 ]
--- app/blogengine/blog/views.py
+++ app/blogengine/blog/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render, get_object_or_404, redirect
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
 from .models import Post, Tag
 from django.views.generic import View
 from .utils import ObjectDetailMixin, ObjectCreateMixin, ObjectUpdateMixIn
@@ -19,6 +21,7 @@ class PostDetail(ObjectDetailMixin, View):
 class PostCreate(ObjectCreateMixin, View):
     form_model = PostForm
     template = 'blog/post_create_form.html'
+
 
 class PostUpdate(ObjectUpdateMixIn, View):
     model = Post
@@ -45,16 +48,14 @@ class TagUpdate(ObjectUpdateMixIn, View):
     model = Tag
     model_form = TagForm
     template = 'blog/tag_update_form.html'
-    # def get(self, request, slug):
-    #     tag = Tag.objects.get(slug__iexact=slug)
-    #     bound_form = TagForm(instance=tag)
-    #     return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag': tag})
-    #
-    # def post(self, request, slug):
-    #     tag = Tag.objects.get(slug__iexact=slug)
-    #     bound_form = TagForm(request.POST, instance=tag)
-    #
-    #     if bound_form.is_valid():
-    #         new_tag = bound_form.save()
-    #         return redirect(new_tag)
-    #     return render(request, 'blog/tag_update_form.html', context={'form': bound_form, 'tag': tag})
+
+
+class TagDelete(View):
+    def get(self, request, slug):
+        tag = Tag.objects.get(slug__iexact=slug)
+        return render(request, 'blog/tag_delete_form.html', context={'tag': tag})
+
+    def post(self, request, slug):
+        tag = Tag.objects.get(slug__iexact=slug)
+        tag.delete()
+        return redirect(reverse('tags_list_url'))
